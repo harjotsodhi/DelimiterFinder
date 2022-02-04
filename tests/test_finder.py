3b4c50d5808fb55f8d60414ca08005eb900fffc4
Harjot Sodhi <harjotsodhi17@gmail.com>
2022-02-04 00:30:04 -0500

    initialize unit tests

---
+++ tests/test_finder.py
@@ -0,0 +1,35 @@
+import unittest
+from DelimiterFinder.finder import Finder
+
+
+class TestFinder(unittest.TestCase):
+
+	def test_single_delim(self):
+		pass
+
+	def test_multi_delim(self):
+		pass
+
+	def test_uncertain(self):
+		pass
+
+	def test_posterior(self):
+		pass
+
+	def test_read_str(self):
+		pass
+
+	def test_read_list(self):
+		pass
+
+	def test_read_path(self):
+		pass
+
+	def test_ignore_chars(self):
+		pass
+
+	def test_ispath(self):
+		pass
+
+	def test_num_samples(self):
+		pass
